6fc30268823d3eceed26ce41b22792f511b24cfe
Gustavo <gustavofeitosa2000@hotmail.com>
2024-08-07 16:24:56 -0300

    Arquivos

---
+++ Funções/saudacao.js
@@ -0,0 +1,8 @@
+function saudacao() {
+    let nome = prompt('Digite seu nome')
+    console.log('Seja bem vindo ' + nome + '!')
+    document.write('Seja bem vindo ' + nome + '!')
+
+}
+
+saudacao()
