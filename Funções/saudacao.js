a73895f12a71561f075eb3cf239cf9ec440e6baa
GUSTAVO FEITOSA DE OLIVEIRA <gustavo.foliveira36@senacsp.edu.br>
2024-08-16 21:49:16 -0300

    Arquivos

--- Funções/saudacao.js
+++
@@ -1,8 +0,0 @@
-function saudacao() {
-    let nome = prompt('Digite seu nome')
-    console.log('Seja bem vindo ' + nome + '!')
-    document.write('Seja bem vindo ' + nome + '!')
-
-}
-
-saudacao()
